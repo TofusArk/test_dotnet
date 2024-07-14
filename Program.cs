6597772f05fc82a1841e63e882c7bb515030e171
Samiyah Hoque <77641931+SamiyahHoque@users.noreply.github.com>
2024-07-14 15:45:37 -0400

    basic

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleDotnetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
