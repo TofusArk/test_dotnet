d7b7a3a0f1d07ce72d17640602cd1ade27492f29
Samiyah Hoque <77641931+SamiyahHoque@users.noreply.github.com>
2024-07-14 16:36:27 -0400

    Testing if it works w version addition

--- obj/Debug/net8.0/test_dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/test_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6597772f05fc82a1841e63e882c7bb515030e171")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
