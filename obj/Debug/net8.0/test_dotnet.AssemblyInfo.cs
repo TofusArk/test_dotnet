0427b14013315f312b6bdb0b54f8e30823b529b3
Samiyah Hoque <77641931+SamiyahHoque@users.noreply.github.com>
2024-07-14 19:16:17 -0400

    check that we can assume bin

--- obj/Debug/net8.0/test_dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/test_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2a803885413a3eec38b6ea7ab65b1579c81572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdfb7af68bf372d07b776da26d562244636b5030")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
